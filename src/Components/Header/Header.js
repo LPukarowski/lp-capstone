704c4a02ef3b8d01c81d73fbb8c1ab9ca143b411
Logan Pukarowski <l.pukarowski15@gmail.com>
2023-06-19 22:31:44 -0400

    header framework

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,15 +2,14 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div className="header">
-            <img src='#' alt='capstone app logo' className="header__logo"/>
+        <header className="header">
             <div className="header__navi">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/create/item">Item</NavLink>
                 <NavLink to="/create/monster">Monster</NavLink>
                 <NavLink to="/:user">Your Library</NavLink>
             </div>
-        </div>
+        </header>
     );
 };
 
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -5,7 +5,18 @@ const HomePage = () => {
     const [ user, setUser ] = useState({})
     return (
         <div>
-            
+            <div>
+                <img src='#' alt='capstone app logo' className="header__logo"/>
+                <h1>WEBSITE NAME</h1>
+                <img src='#' alt='cpastone app logo' className="header__logo"/>
+            </div>
+            <div>
+                <p className="header__web-desc"></p>
+            </div>
+            <div className="header__cta-buttons">
+                <button>Sign Up Now!</button>
+                <button>Start Your Library</button>
+            </div>
         </div>
     );
 };
